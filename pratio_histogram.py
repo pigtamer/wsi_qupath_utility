30ef8eded2786c91534e00ad52e68c68dfc1a07b
cunyuan <cygee99@gmail.com>
2020-10-01 13:11:25 +0900

    2020年 10月  1日 木曜日 13:11:24 JST  at lab

--- pratio_histogram.py
+++ pratio_histogram.py
@@ -1,24 +1,32 @@
+#%%
 import cv2 as cv
 from numpy import *
 import numpy as np
 import matplotlib.pyplot as plt
 import os, sys, glob
-
+#%%
 w_tar, h_tar = 256, 256
+# w_tar, h_tar = 2048, 2048
 
 basepath = "/home/cunyuan/4tb/Kimura/DATA"
-dpl = {"IHC": basepath+ "/TILES_(%d, %d)/DAB/*/*/*/" % (size, size),
-       "Masks": basepath + "/TILES_(%d, %d)/Masks/*/*/*/" % (size, size)}
+dpl = {"DAB": basepath+ "/TILES_(%d, %d)/DAB/*/*/Tumor/*" % (w_tar, h_tar),
+       "Mask": basepath + "/TILES_(%d, %d)/Mask/*/Tiles/Tumor/*" % (w_tar, h_tar),
+       "HE": basepath + "/TILES_(%d, %d)/HE/*/Tiles/Tumor/*" % (w_tar, h_tar),
+       "IHC": basepath + "/TILES_(%d, %d)/IHC/*/Tiles/Tumor/*" % (w_tar, h_tar),
+       }
        
 
 # dpl = [for x in dpl]
 
 k = 0
 l = []
-dp = "Masks"
+dp = "Mask"
 
-for m in glob.glob(dpl[dp] + "/*.tif"):
-    # print(c,d,m,n)
+#%%
+# for m in glob.glob(dpl[dp] + "/*.png"):
+#     # print(c,d,m,n)
+for c, d, m, n in zip(*[sort(glob.glob(dpl[dp] + "/*.tif")) for dp in dpl.keys()]):
+    print(c,"\n", d, "\n", m, "\n", n)
     k+=1
     imm = cv.imread(m).astype(np.uint8)
     imm = cv.cvtColor(imm, cv.COLOR_BGR2GRAY)/255
@@ -27,5 +35,34 @@ for m in glob.glob(dpl[dp] + "/*.tif"):
     nw, nh = w//w_tar, h//h_tar
     
     l.append(np.sum(imm) / (w*h))
+#%%
+fig=plt.figure(figsize=(8,6), dpi=300)
+ax = fig.add_subplot(1, 1, 1)
 
+# Major ticks every 20, minor ticks every 5
+major_ticks = np.arange(0, 1000, 100)
+minor_ticks = np.arange(0, 1000, 10)
+
+hl = ax.hist(l, bins=128, rwidth=0.5)
+
+xlim = np.max(l)
+ylim = np.max(hl[0])
+
+ax.set_xticks(major_ticks*xlim/1000)
+ax.set_xticks(minor_ticks*xlim/1000, minor=True)
+ax.set_yticks(major_ticks*ylim/1000)
+ax.set_yticks(minor_ticks*ylim/1000, minor=True)
+
+
+# Or if you want different settings for the grids:
+ax.grid(which='minor', alpha=0.2)
+ax.grid(which='major', alpha=0.5)
+# And a corresponding grid
+# ax.grid(which='both')
+
+plt.tight_layout()
+plt.title("Histogram of + pixel ratio in the Masks")
+plt.show()
 print(k)
+
+# %%
